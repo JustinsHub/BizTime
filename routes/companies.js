2f5d4d9ebdd25249dbb9593e0311c1a8509e4a85
Justin Z <contactjustinzamora@gmail.com>
2020-12-18 21:57:39 -0800

    added testing database and routes

--- routes/companies.js
+++ routes/companies.js
@@ -7,7 +7,7 @@ const ExpressError = require('../expressError');
 router.get('/', async (req, res, next)=>{
     try{
     const results = await db.query('SELECT * FROM companies')
-    return res.send({companies:[results.rows]})
+    return res.send({companies: results.rows})
     }catch(err){
         next(err)
     }
@@ -31,9 +31,6 @@ router.post('/', async(req, res, next)=>{
     const {code, name, description} = req.body
     const results = await db.query('INSERT INTO companies (code, name, description) VALUES ($1,$2,$3) RETURNING *',
                                     [code, name, description])
-    if(results.rows.length === 0){
-        throw new ExpressError('Must fill in the input', 404)
-    }
     return res.status(201).json({company: results.rows[0]})
     }catch(err){
         next(err)
@@ -42,23 +39,35 @@ router.post('/', async(req, res, next)=>{
 
 router.patch('/:code', async(req,res,next)=>{
     try{
-    const {code} = req.params
-    const {name, description} = req.body
-    const results = await db.query('UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *',
-                                [name, description, code])
-    return res.json({company: results.row[0]})
+        const {code} = req.params
+        const {name, description} = req.body
+        const results = await db.query(
+            `UPDATE companies
+            SET name=$1, description=$2
+            WHERE code = $3
+            RETURNING code, name, description`,
+        [name, description, code]);
+        if(results.rows.length === 0){
+            throw new ExpressError(`Can't find ${code}`, 404)
+        }
+        return res.json({company: results.rows[0]})
     }catch(err){
         next(err)
     }
 })
 
+
 router.delete('/:code', async(req,res,next)=>{
     try{
-    const {code} = req.params
-    const results = await db.query('DELETE FROM companies WHERE code=$1', [code])
-    return res.json({status: "Deleted"})
+        const {code} = req.params
+        const results = await db.query('DELETE FROM companies WHERE code=$1 RETURNING *', [code])
+        if(results.rows.length == 0){
+            throw new ExpressError(`Can't delete. Can't find ${code}`, 404)
+        }
+        return res.send({status: "Deleted"})
     }catch(err){
         next(err)
     }
 })
+
 module.exports = router;
---
+++ tests/companies.test.js
@@ -0,0 +1,72 @@
+process.env.NODE_ENV = "test"
+const request = require('supertest')
+const app = require('../app')
+const db = require('../db')
+
+let testCompany;
+
+beforeEach(async ()=>{
+    const result = await db.query(`INSERT INTO companies (code, name, description)
+                                    VALUES ('Dogs', 'Dexter', 'GoldenDoodle') RETURNING code, name, description`)
+    testCompany = result.rows[0]
+})
+
+afterEach(async ()=>{
+    await db.query('DELETE FROM companies')
+})
+
+afterAll(async ()=>{
+    await db.end()
+})
+
+describe('GET /companies', ()=>{
+    test('Get all companies', async ()=>{
+        const res = await request(app).get('/companies')
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({companies: [testCompany]})
+    })
+})
+
+describe('GET /companies/:code', ()=>{
+    test('Get single company', async()=>{
+        const res = await request(app).get(`/companies/${testCompany.code}`)
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({company: testCompany})
+    })
+    test('Respond 404 if company is missing', async()=>{
+        const res = await request(app).get(`/companies/0`)
+        expect(res.statusCode).toBe(404)
+    })
+})
+
+describe('POST /companies', ()=>{
+    test('Create a new company', async()=>{
+        const res = await request(app).post('/companies').send({code:"TSLA", name:"Tesla", description:"Future cars"})
+        expect(res.statusCode).toBe(201)
+        expect(res.body).toEqual({company: {code:expect.any(String), name:"Tesla", description:"Future cars"}})
+    })
+})
+
+describe('PATCH /companies/:code', ()=>{
+    test('Updating a company', async()=>{
+        const res = await request(app).patch(`/companies/${testCompany.code}`).send({name:"Facebook", description:"A Website"})
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({company: {code:testCompany.code, name:"Facebook", description:"A Website"}})
+    })
+    test('Respond 404 if company is missing', async()=>{
+        const res = await request(app).patch(`/companies/0`).send({name:"Facebook", description:"A Website"})
+        expect(res.statusCode).toBe(404)
+    })
+})
+
+describe('DELETE /companies/:code', ()=>{
+    test('Delete a company', async()=>{
+        const res = await request(app).delete(`/companies/${testCompany.code}`)
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toEqual({status: "Deleted"})
+    })
+    test('Respond 404 if company is missing', async()=>{
+        const res = await request(app).delete('/companies/poop')
+        expect(res.statusCode).toBe(404)
+    })
+})
